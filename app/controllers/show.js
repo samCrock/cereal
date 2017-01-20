e8b30ff3d8f8f1767d70051e6713449b3754faa6
Sam <samuele.lombardi@tutored.it>
2017-01-20 22:17:14 +0100

    Season fixes & better layout

--- app/controllers/show.js
+++ app/controllers/show.js
@@ -5,24 +5,31 @@
         .module('app')
         .controller('showCtrl', showCtrl);
 
-    function showCtrl($rootScope, $state, $scope, $interval, $stateParams, jsonService, torrentService, subsService, commonService, wtService, messageService) {
+    function showCtrl($rootScope, $state, $scope, $interval, $stateParams, jsonService, torrentService, subsService, commonService, wtService, dialogService) {
 
         let fsExtra = require('fs-extra')
 
         $rootScope.loading = true
         console.log('$stateParams', $stateParams)
-        $scope.title = commonService.capitalCase($stateParams.show.trim())
-        $rootScope.current_show = $scope.title
+        $scope.title = $stateParams.show ? commonService.capitalCase($stateParams.show.trim()) : $rootScope.current_show.Title
+        $scope.selected_ep = $stateParams.episode
+        $rootScope.current_show.Title = $scope.title
         $scope.downloading = []
-        console.log($scope.title)
 
-        $rootScope.$on('completed', (showObj) => {
-            start()
+
+        // $rootScope.$on('completed', (showObj) => {
+        //     start()
+        // })
+
+        $rootScope.$on('show_ready', (e, showResult) => {
+            formatDataAndSave(showResult)
+            $scope.showIsLoading = false
+            console.log('Show is ready')
         })
 
         $scope.dotClass = function(episode) {
             if (episode.downloaded) return 'green'
-            if (episode.timePassed && episode.timePassed.indexOf('ago') === -1) return 'hidden'
+            if (episode.timePassed && episode.timePassed.indexOf('ago') === -1 && episode.timePassed.indexOf('Today') === -1) return 'hidden'
             return ''
         }
 
@@ -30,21 +37,22 @@
             console.log('PLAY ->', episode)
             torrentService.searchTorrent({
                     show: $scope.title,
-                    episode: episode.episode
+                    episode: episode.label
                 })
                 .then((t) => {
-                    let streamObj = { magnet: t.magnet, path: process.cwd() + '/library/' + $scope.title + '/' + episode.episode }
+                    let streamObj = { magnet: t.magnet, path: process.cwd() + '/library/' + $scope.title + '/' + episode.label }
                     console.log('streamObj', streamObj)
                     commonService.stream(streamObj)
                 })
                 .catch((reason) => {
                     console.log(reason)
-                    messageService.notify({ title: 'Sorry', content: 'No results' })
+                    dialogService.notify({ title: 'Sorry', content: 'No results' })
                 })
         }
 
         $scope.play = function(episode) {
-            if (episode.downloaded) $state.go('app.episode', ({ show: $scope.title, episode: episode.episode }))
+            console.log('play', episode.label)
+            $state.go('app.episode', ({ show: $scope.title, episode: episode.label }))
         }
 
         $interval(() => {
@@ -55,53 +63,73 @@
                             if ($rootScope.pending[i].show === $scope.show.Title &&
                                 $rootScope.pending[i].episode === $scope.show.Seasons[s][e].episode) {
                                 $scope.show.Seasons[s][e].eta = commonService.formatTime($rootScope.pending[i].eta)
-                                console.log('downloading', $scope.show.Seasons[s][e].episode)
+                                $scope.$applyAsync()
                             }
                         }
                     }
                 }
             }
+
             if (!$scope.$$phase) {
                 $scope.$apply()
             }
         }, 1000)
 
         function start() {
+            if (sessionStorage.getItem('current_show')) {
+                let tmp = JSON.parse(sessionStorage.getItem('current_show'))
+                if (tmp.Title === $scope.title) {
+                    console.log('Found', $scope.title, 'in memory')
+                    $scope.show = tmp
+                    $rootScope.loading = false
+                    return
+                }
+            }
             jsonService.getShow($scope.title)
                 .then((showResult) => {
-                    var formatted_date = {}
-                    console.log('showResult', showResult)
-                    if (showResult) {
-                        for (var season in showResult.Seasons) {
-                            for (var episode in showResult.Seasons[season]) {
-                                formatted_date = commonService.getDayObject(showResult.Seasons[season][episode].date)
-                                showResult.Seasons[season][episode].dotw = formatted_date.dotw
-                                showResult.Seasons[season][episode].dotm = formatted_date.dotm
-                                showResult.Seasons[season][episode].month = formatted_date.month
-                                showResult.Seasons[season][episode].timePassed = commonService.timePassed(showResult.Seasons[season][episode].date)
-                                JSON.parse(localStorage.getItem('library')).filter((d) => {
-                                    if (d.show === showResult.Title && d.episode === showResult.Seasons[season][episode].episode && !showResult.Seasons[season][episode].eta) {
-                                        showResult.Seasons[season][episode].downloaded = true
-                                    }
-                                })
-                            }
-                        }
-                    }
+                    console.log('Found', $scope.title, 'in local json')
                     $scope.show = showResult
-                    $scope.selectedIndex = 1
-
+                    formatDataAndSave(showResult)
                     $rootScope.loading = false
-                    $scope.$apply()
+
                 }).catch((errorMsg) => {
-                    console.error(errorMsg)
+                    // console.error('Something went south..')
                 })
         }
 
-        $rootScope.$on('show_overview', function(event, result) {
+        function formatDataAndSave(showResult) {
+            console.log('formatDataAndSave', showResult)
+            if (showResult) {
+                for (var season in showResult.Seasons) {
+                    for (var episode in showResult.Seasons[season]) {
+                        var formatted_date = {}
+                        formatted_date = commonService.getDayObject(showResult.Seasons[season][episode].date)
+                        showResult.Seasons[season][episode].dotw = formatted_date.dotw
+                        showResult.Seasons[season][episode].dotm = formatted_date.dotm
+                        showResult.Seasons[season][episode].month = formatted_date.month
+                        showResult.Seasons[season][episode].timePassed = commonService.timePassed(showResult.Seasons[season][episode].date)
+                        JSON.parse(localStorage.getItem('library')).filter((d) => {
+                            if (d.show === showResult.Title && d.episode === showResult.Seasons[season][episode].episode && !showResult.Seasons[season][episode].eta) {
+                                showResult.Seasons[season][episode].downloaded = true
+                            }
+                        })
+                    }
+                }
+            }
+            $scope.show = showResult
+            $rootScope.current_show = $scope.show
+            sessionStorage.setItem('current_show', JSON.stringify($scope.show))
+            $scope.selectedIndex = 1
+            $scope.$applyAsync()
+
+        }
+
+        $scope.$on('show_overview', function(event, result) {
             console.log('Overview ready!')
             $scope.show = result.show
             $rootScope.loading = false
-            $scope.$apply()
+            $scope.showIsLoading = true
+            $scope.$applyAsync()
         })
 
         $scope.downloadEpisode = (episode) => {
@@ -121,21 +149,45 @@
                     episode: label
                 })
                 .then((result) => {
+                    console.log(result.episode, 'is downloading')
+                    $scope.show.Seasons[s][e].loading = false
                     torrentService.downloadTorrent(result, $rootScope)
                         .then((t) => {
+                            delete $scope.show.Seasons[s][e].eta
+                            $rootScope.current_show = $scope.show
+                            sessionStorage.setItem('current_show', JSON.stringify($scope.show))
                             $scope.show.Seasons[s][e].loading = false
-                            console.log(result.episode, 'is downloading')
+                            $scope.show.Seasons[s][e].downloaded = true
+                            console.log(result.episode, 'is ready')
+                            $scope.showIsLoading = false
+                            $scope.$applyAsync()
                         })
                 })
                 .catch((reason) => {
-                    $scope.show[s][e].loading = false
                     console.log('No torrent was found')
+                    console.log($scope.show, s, e)
+                    $scope.show.Seasons[s][e].loading = false
+                    dialogService.torrentForm({ show: $scope.show.Title, episode: $scope.show.Seasons[s][e].episode, date: $scope.show.Seasons[s][e].date })
+                        .then((result) => {
+                            console.log('Dialog result ->', result)
+                        })
+                        .catch(() => {
+                            console.log('Dialog closed')
+                        })
                 })
         }
 
+        $scope.playTrailer = () => {
+            let trailer = $scope.show.Trailer
+            console.log('Playing trailer:', trailer)
+
+            dialogService.trailer({ src: $scope.show.Trailer })
+
+        }
+
         $scope.deleteEpisode = (showObj) => {
-            let show = showObj.show
-            let episode = showObj.episode
+            let show = $scope.title
+            let episode = showObj.label
             console.log('Deleting actual folder')
             fsExtra.removeSync(process.cwd() + '/library/' + show + '/' + episode);
             console.log('Deleting from library (rootscope)')
