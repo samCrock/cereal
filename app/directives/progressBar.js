74f263e257b0ce5e27f66cde7ff365d9cf548701
Sam <samuele.lombardi@tutored.it>
2017-03-04 13:50:55 +0100

    ProgressBar directive

---
+++ app/directives/progressBar.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('progressBar', progressBar)
+
+    /* @ngInject */
+    function progressBar() {
+
+        var directive = {
+            link: link,
+            scope: {
+                progress: '='
+            },
+            restrict: 'E',
+            templateUrl: 'app/directives/progressBar.html'
+        }
+        return directive
+
+        function link(scope) {
+            // console.log('progressBar scope:', scope.progress)
+            scope.progress_label = scope.progress ? scope.progress + '%' : '0'
+        }
+    }
+})();
