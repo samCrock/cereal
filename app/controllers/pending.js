99f4c050f16ce07e596ff89ce570d70d3c0b254b
Sam <samuele.lombardi@tutored.it>
2017-03-17 14:31:13 +0100

    Fix delete pending

--- app/controllers/pending.js
+++ app/controllers/pending.js
@@ -11,24 +11,33 @@
         const wt_client = wtService.client()
         let fsExtra = require('fs-extra')
 
+        var deleteFromPending = (torrent) => {
+            for (var i = 0; i < $rootScope.pending.length; i++) {
+                if ($rootScope.pending[i].show === torrent.show && $rootScope.pending[i].episode === torrent.episode) {
+                    $rootScope.pending.splice(i, 1)
+                }
+            }
+            let local_pending = JSON.parse(localStorage.getItem('pending'))
+            for (var i = 0; i < local_pending.length; i++) {
+                if (local_pending[i].show === torrent.show && local_pending[i].episode === torrent.episode) {
+                    local_pending.splice(i, 1)
+                }
+            }
+            localStorage.setItem('pending', JSON.stringify(local_pending))
+            $rootScope.$applyAsync()
+        }
+
         $scope.remove = (torrent) => {
             console.log(torrent);
-            wt_client.remove(torrent.magnet, () => {
-                console.log('Deleted from client')
-                for (var i = 0; i < $rootScope.pending.length; i++) {
-                    if ($rootScope.pending[i].magnet === torrent.magnet) {
-                        $rootScope.pending.splice(i, 1)
-                    }
-                }
-                let local_pending = JSON.parse(localStorage.getItem('pending'))
-                for (var i = 0; i < local_pending.length; i++) {
-                    if (local_pending[i].magnet === torrent.magnet) {
-                        local_pending.splice(i, 1)
-                    }
-                }
-                localStorage.setItem('pending', JSON.stringify(local_pending))
-            })
-
+            if (wt_client.get(torrent.magnet)) {
+                wt_client.remove(torrent.magnet, () => {
+                    console.log('Deleted from client')
+                    deleteFromPending(torrent)
+                })
+            } else {
+                console.log('No torrent in wt');
+                deleteFromPending(torrent)
+            }
         }
 
     }
