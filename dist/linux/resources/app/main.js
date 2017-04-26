392dc73c2a6df30288b083ab99f6e80591bf9b44
Sam <samuele.lombardi@tutored.it>
2017-04-26 15:01:27 +0200

    Reinstated custom updater
    Former-commit-id: ae54bbc9851cd823c923a529dbd74381ecfd0a67

--- dist/linux/resources/app/main.js
+++ dist/linux/resources/app/main.js
@@ -1,10 +1,10 @@
 const electron = require('electron')
 const path = require('path')
-const https = require('https');
+const https = require('https')
 
-const fs = require('fs');
+const fs = require('fs')
 
-const localAppVersion = require('./package.json').version;
+const localAppVersion = require('./package.json').version
 // Module to control application life.
 const app = electron.app
     // Module to create native browser window.
@@ -14,11 +14,10 @@ const os = require('os')
 const fsExtra = require('fs-extra')
 
 // const log = require('electron-log');
-const {autoUpdater} = require('electron-updater');
+const {autoUpdater} = require('electron-updater')
 
-
-const PATH_APP_NODE_MODULES = path.join(__dirname, '..', '..', 'app', 'node_modules');
-require('module').globalPaths.push(PATH_APP_NODE_MODULES);
+const PATH_APP_NODE_MODULES = path.join(__dirname, '..', '..', 'app', 'node_modules')
+require('module').globalPaths.push(PATH_APP_NODE_MODULES)
 
 console.log('Current OS arch:', os.arch())
 console.log('Current OS platform:', os.platform())
@@ -30,24 +29,24 @@ let mainWindow
 function createWindow() {
 
     /////////////
-    // https.get('https://raw.githubusercontent.com/samCrock/cereal/master/package.json', (res) => {
-    //     var body = ''
-    //     res.on('data', (d) => {
-    //         body += d
-    //     });
-    //     res.on('end', () => {
-    //         var packageContent = JSON.parse(body);
-    //         process.stdout.write('Remote version ->  ' + packageContent.version);
-    //         if (packageContent.version !== localAppVersion) {
-    //             process.stdout.write('\nNeed an update pal?');
-    //             global.config = {
-    //                 update: true
-    //             };
-    //         }
-    //     });
-    // }).on('error', (e) => {
-    //     console.error(e);
-    // });
+    https.get('https://raw.githubusercontent.com/samCrock/cereal/master/package.json', (res) => {
+        var body = ''
+        res.on('data', (d) => {
+            body += d
+        });
+        res.on('end', () => {
+            var packageContent = JSON.parse(body);
+            process.stdout.write('Remote version ->  ' + packageContent.version);
+            if (packageContent.version !== localAppVersion) {
+                process.stdout.write('\nNeed an update pal?');
+                global.config = {
+                    update: true
+                };
+            }
+        });
+    }).on('error', (e) => {
+        console.error(e);
+    });
     ///////////////
 
     console.log('Local version  -> ', localAppVersion);
@@ -81,47 +80,51 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', function() {
-    autoUpdater.checkForUpdates()
+    // autoUpdater.checkForUpdates()
     createWindow()
 })
 
-autoUpdater.on('update-downloaded', (ev, info) => {
-    // Wait 5 seconds, then quit and install
-    // In your application, you don't need to wait 5 seconds.
-    // You could call autoUpdater.quitAndInstall(); immediately
-    console.log('update-downloaded')
-    setTimeout(function() {
-        autoUpdater.quitAndInstall();
-    }, 5000)
-})
+// autoUpdater.on('update-downloaded', (ev, info) => {
+//     // Wait 5 seconds, then quit and install
+//     // In your application, you don't need to wait 5 seconds.
+//     // You could call autoUpdater.quitAndInstall(); immediately
+//     console.log('update-downloaded')
+//     setTimeout(function() {
+//         autoUpdater.quitAndInstall();
+//     }, 5000)
+// })
+
+// autoUpdater.on('checking-for-update', (ev, info) => {
+//     console.log('checking-for-update', info)
+// })
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    // process.stdout.write('\nQuitting..');
-    // let fileName = 'Cereal-0.1.1.deb'
-    // fs.readFile(fileName, (err) => {
-    //     if (err) {
-    //         process.stdout.write('No updates available');
-    //     } else {
-    //         process.stdout.write('\nUpdating...');
-    //         let util = require('util');
-    //         let exec = require('child_process').exec;
-    //         let platform = os.platform()
-    //         switch (process.platform) {
-    //             case 'darwin':
-    //                 return exec('open "' + fileName + '"')
-    //             case 'win32':
-    //                 return shell.openExternal('"' + fileName + '"')
-    //             case 'win64':
-    //                 return shell.openExternal('"' + fileName + '"')
-    //             default:
-    //                 return exec('sudo dpkg -i ./Cereal-0.1.1.deb')
-    //         }
+    process.stdout.write('\nQuitting..');
+    let fileName = 'Cereal-0.1.1.deb'
+    fs.readFile(fileName, (err) => {
+        if (err) {
+            process.stdout.write('No updates available');
+        } else {
+            process.stdout.write('\nUpdating...');
+            let util = require('util');
+            let exec = require('child_process').exec;
+            let platform = os.platform()
+            switch (process.platform) {
+                case 'darwin':
+                    return exec('open "' + fileName + '"')
+                case 'win32':
+                    return shell.openExternal('"' + fileName + '"')
+                case 'win64':
+                    return shell.openExternal('"' + fileName + '"')
+                default:
+                    return exec('sudo dpkg -i ./Cereal-0.1.1.deb')
+            }
 
-    //     }
-    // });
+        }
+    });
     if (process.platform !== 'darwin') {
         app.quit()
     }
